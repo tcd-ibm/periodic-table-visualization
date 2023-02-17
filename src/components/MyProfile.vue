9946d78adf3482031f6b5dc2970bcc3bcb110fc8
padmored <padmored@tcd.ie>
2023-02-17 15:36:26 +0000

    title added

---
+++ src/components/MyProfile.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="home row">
+        <h3>&ltusername&gt's profile</h3>
+        <div class="c-periodic-table">
+        <div v-for="element in elements"
+            :data-element-group='element.elementGroup' :data-group='element.group' :data-period='element.period'
+            class='element' :class="element.symbol && element.symbol.toLowerCase()"
+            :style="{ opacity: filteredElements.includes(element.atomicNumber) ? 1 : 0.25 }">
+            <router-link :to="'/element/' + element.atomicNumber">
+            <element-definition class="u-aspect-ratio" :element="element" :detailed="true"></element-definition>
+            </router-link>
+        </div>
+    
+        <div class="element lanthanoid" data-element-group="lanthanoid" :style="{ opacity: filteredElementsContainElementsOfGroup('lanthanoid') ? 1 : 0.25 }"></div>
+        <div class="element actinoid" data-element-group="actinoid" :style="{ opacity: filteredElementsContainElementsOfGroup('actinoid') ? 1 : 0.25 }"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import ElementBadge from './ElementBadge'
+  import ElementDefinition from './ElementDefinition'
+  import ElementGeneralProperties from './ElementProfile/ElementGeneralProperties'
+
+  export default {
+    name: 'periodic-table',
+    components: {
+      ElementGeneralProperties, ElementDefinition, ElementBadge
+    },
+    computed: {
+      ...mapGetters({
+        elements: 'localizedElements',
+        filteredElements: 'filteredElements'
+      }),
+      selectedElement () {
+        return this.elements[this.selectedElementId] || {}
+      }
+    },
+    data () {
+      return {
+        selectedElementId: '',
+        showInfo: false
+      }
+    },
+    methods: {
+      showElement (element) {
+        this.showInfo = true
+        this.selectedElementId = element.atomicNumber
+      },
+      hideElement () {
+        this.showInfo = false
+      },
+      filteredElementsContainElementsOfGroup (group) {
+        let filteredElementsContainElementsOfGroup = false
+        for (let id of this.filteredElements) {
+          if (this.elements[id].elementGroup === group) {
+            filteredElementsContainElementsOfGroup = true
+          }
+        }
+        return filteredElementsContainElementsOfGroup
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/scss/components/myProfile-table";
+</style>
---
+++ src/components/Sidebar/MyProfileOptions.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="c-periodic-table-options">
+      <button v-for="group in groups" type="button"
+              @mouseover="addGroupOnHover(group)" @mouseout="removeGroupOnHover(group)"
+              class="btn btn-primary btn-block" :class="'btn-' + group">{{ $t("element.group." + group) }}
+  
+      </button>
+  
+      <p class="text-muted mt-3">{{ $t("sidebar.simpleView") }}</p>
+      <mdc-switch v-model="simpleView"></mdc-switch>
+    </div>
+</template>
+
+<script>
+  import * as types from '@/store/mutation-types'
+  import MdcSwitch from '../shared/MaterialComponents/MdcSwitch'
+
+  export default {
+    name: 'periodic-table-options',
+    components: {
+      MdcSwitch
+    },
+    data () {
+      return {
+        groups: [
+          'non-metal',
+          'halogen',
+          'metalloid',
+          'metal',
+          'alkali-metal',
+          'alkaline-earth-metal',
+          'transition-metal',
+          'noble-gas',
+          'lanthanoid',
+          'actinoid'
+        ]
+      }
+    },
+    methods: {
+      addGroupOnHover (group) {
+        this.$store.commit(types.ADD_GROUP, group)
+      },
+      removeGroupOnHover (group) {
+        setTimeout(() => this.$store.commit(types.REMOVE_GROUP, group), 125)
+      }
+    },
+    computed: {
+      simpleView: {
+        get () {
+          return this.$store.state.configuration.simpleView
+        },
+        set (value) {
+          this.$store.commit(types.TOGGLE_SIMPLE_VIEW, value)
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/scss/trumps/colors';
+
+  @each $name, $color in $groups {
+    .btn-#{$name} {
+      background-color: rgba($color, 0.25);
+      border: none;
+      text-align: left;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+
+      &:hover {
+        background-color: rgba($color, 0.5);
+      }
+    }
+  }
+</style>
