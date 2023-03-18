8a52dd00014d5b4399035fed11373d88c4ee9a2b
andrewsyo <andrewsyomush@gmail.com>
2023-03-18 18:57:58 +0000

    logout on tab close

--- src/components/Logout.vue
+++ src/components/Logout.vue
@@ -1,22 +1,20 @@
 <template>
   <div>
-  <div>
-    Would you like to logout?
-  </div>
-  <div>
-    <button type="button"
-      class="btn btn-primary" @click="logout()">Logout</button>
-    <router-link :to="{ path: './Home' }"><button type="submit" class="btn btn-primary">Go home</button></router-link>
-      </div>
+    <div>
+      Would you like to logout?
+    </div>
+    <div>
+      <button type="button" class="btn btn-primary" @click="logout()">Logout</button>
+      <router-link :to="{ path: './Home' }"><button type="submit" class="btn btn-primary">Go home</button></router-link>
+    </div>
   </div>
 </template>
 
 <script>
-
 import { mapActions } from 'vuex'
+
 export default {
-  methods:
-  {
+  methods: {
     ...mapActions(['logoutUser']),
     async logout () {
       console.log('logout() is called in logout.vue')
@@ -26,19 +24,14 @@ export default {
       console.log(username)
       await this.logoutUser()
       this.$router.push('Login')
-    },
-    mounted () {
-      window.addEventListener('beforeunload', (event) => {
-        event.preventDefault()
-        event.returnValue = ''
-        this.logout()
-      })
-    },
-    beforeDestroy () {
-      window.removeEventListener('beforeunload', () => {
-        this.logout()
-      })
     }
+  },
+  mounted () {
+    window.addEventListener('beforeunload', this.logout)
+    console.log('HELLO')
+  },
+  beforeDestroy () {
+    window.removeEventListener('beforeunload', this.logout)
   }
 }
 </script>
