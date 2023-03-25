967ce08d1c5e26167cc23c77d90f1f2d1ed06d36
Andrew Syomushkin <123475751+syomusha@users.noreply.github.com>
2023-03-25 14:25:05 +0000

    Update App.vue
    current app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
-    <div id="app" class="app container-fluid">
-      <router-view></router-view>
-    </div>
+  <div id="app" class="app container-fluid">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+
 export default {
   name: 'app',
   methods: {
     ...mapActions(['sendChangePasswordEmail', 'logoutUser'])
   },
-  created () {
-    // need to find a way to distinguish page refresh and browser/tab close
-    window.addEventListener('beforeunload', this.logoutUser())
+  mounted () {
+    // Check if the token is present in local storage
+    const token = this.$store.getters.getAuthToken
+    if (token === '') {
+      // If the token is not present, log the user out
+      this.logoutUser()
+    }
   }
 }
 </script>
 
 <style lang="scss">
-@import './assets/scss/design.scss';
+@import './assets/scss/design.scss'
 </style>
