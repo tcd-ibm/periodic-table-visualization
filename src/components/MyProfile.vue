b033543dd7b26ec54fe117520537f6768dc5af54
Nuoxi Zhang <zhangnuoxi24@gmail.com>
2023-03-27 12:40:59 +0100

    removed else statement

--- src/components/MyProfile.vue
+++ src/components/MyProfile.vue
@@ -19,8 +19,8 @@
       <myProfile-general-properties v-if="Object.keys(selectedElement).length > 0" class="c-information" :element="selectedElement" :removed="removed" :preview="true" ></myProfile-general-properties>
       <div class="c-information"></div>
       <div class="element" :data-element-group="non-metal" v-for="i in 118" v-if="empty(userElements, i - 1)" :key="i"><router-link :to="{ name: 'AddElement', params: { pos: i - 1} }"><br><center><div class="material-icons">add_box</div></center><br></router-link></div>
-      <div :key="element.id" v-else v-for="element in userElements"
-          v-if="!removed.includes(element.symbol)"
+      <div :key="element.id" v-else-if="!empty(userElements, i - 1)" v-for="element in userElements"
+          
           :data-element-group="getGroup(element)" :data-group='element.group'
           class='element' :class="'pos' + getPosition(element)"
           :style="{ opacity: filteredElementsContainElementsOfGroup(element.group) ? 1 : 0.25 }">
