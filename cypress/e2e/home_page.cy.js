41fa989c3ac7f4b237908860db15a575e7b50479
temi-oye <29523551+temi-oye@users.noreply.github.com>
2023-03-31 14:49:02 +0100

    added code to test the clicking of elements on the front page

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -1,5 +1,23 @@
+const { compareSync } = require("bcryptjs")
+
+///
 describe("The home page", () => {
     it("loads successfully", () => {
         cy.visit("")
+
+        cy.get(".element").each(($el, index) =>{
+            // clicks each of the first 5 elements one by one and tries to return to home after clicking each one....
+            if(index == 5){
+                return false;
+            } else{
+                cy.get(".c-element-definition.d-flex.flex-column.justify-content-between.u-aspect-ratio").eq(index).should("exist")
+                cy.get(".c-element-definition.d-flex.flex-column.justify-content-between.u-aspect-ratio").eq(index).click()
+                cy.get(".nav-item").should("be.visible")
+                cy.contains("home").click()
+            }
+
+        })
+        // cy.get("@el").click()
+
     })
 })
